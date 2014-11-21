9d3f56173b23d618c6cbb40e89789c4e3edba607
psquickitjayant <jayant.varshney@psquickit.com>
2014-11-22 00:09:38 +0530

    Minor modifications

--- Modular Scripts/Apache2/configure-apache.sh
+++ Modular Scripts/Apache2/configure-apache.sh
@@ -151,7 +151,7 @@ setApacheVariables()
 checkIfLogRotationEnabled()
 {
 	if [[ $(grep -r "/var/log/$SERVICE/*." /etc/logrotate.d/$SERVICE) ]]; then
-		logMsgToConfigSysLog "WARN" "WARN: Log rotation is enabled on $LOGGLY_APACHE_LOG_HOME/$APACHE_ACCESS_LOG_FILE and $LOGGLY_APACHE_LOG_HOME/$APACHE_ERROR_LOG_FILE."
+		logMsgToConfigSysLog "WARN" "WARN: Log rotation is enabled on $LOGGLY_APACHE_LOG_HOME/$APACHE_ACCESS_LOG_FILE and $LOGGLY_APACHE_LOG_HOME/$APACHE_ERROR_LOG_FILE.  Please follow instructions here to update logrotate https://www.loggly.com/docs/log-rotate"
 	fi
 }
 
