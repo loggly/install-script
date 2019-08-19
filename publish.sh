c33725581910881f0ec45c5deff278088eef604c
Michal Chomo <michal.chomo@solarwinds.cloud>
2019-08-19 10:52:17 +0200

    remove backslashes

--- publish.sh
+++ publish.sh
@@ -5,13 +5,13 @@ copy_to_aws() {
 	aws s3 cp "$1" s3://loggly-install/install/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 }
 
-declare -a files=("Linux\ Script/configure-linux.sh"
-				  "Modular\ Scripts/File\ Monitoring/configure-file-monitoring.sh"
-				  "Modular\ Scripts/Apache2/configure-apache.sh"
-				  "Modular\ Scripts/Nginx/configure-nginx.sh"
-				  "Modular\ Scripts/S3Logs\ Monitoring/configure-s3-file-monitoring.sh"
-				  "Modular\ Scripts/Tomcat/configure-tomcat.sh"
-				  "Mac\ Script/configure-mac.sh"
+declare -a files=("Linux Script/configure-linux.sh"
+				  "Modular Scripts/File Monitoring/configure-file-monitoring.sh"
+				  "Modular Scripts/Apache2/configure-apache.sh"
+				  "Modular Scripts/Nginx/configure-nginx.sh"
+				  "Modular Scripts/S3Logs Monitoring/configure-s3-file-monitoring.sh"
+				  "Modular Scripts/Tomcat/configure-tomcat.sh"
+				  "Mac Script/configure-mac.sh"
 				  "AWSscripts/SQS3script.py")
 
 for file in "${files[@]}";do
