0f8853de2e153c28bcf4d9e136d86e3fe9740794
Gabriel Quirschfeld <quirschfeld@gmail.com>
2020-05-07 12:09:34 +0200

    change script version

--- Modular Scripts/Tomcat/configure-tomcat.sh
+++ Modular Scripts/Tomcat/configure-tomcat.sh
@@ -9,7 +9,7 @@ source configure-linux.sh "being-invoked"
 #name of the current script
 SCRIPT_NAME=configure-tomcat.sh
 #version of the current script
-SCRIPT_VERSION=1.6
+SCRIPT_VERSION=1.7
 
 #minimum version of tomcat to enable log rotation
 MIN_TOMCAT_VERSION=6.0.33.0
